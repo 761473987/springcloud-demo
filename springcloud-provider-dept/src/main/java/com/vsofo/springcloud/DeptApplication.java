504f0bd11662be666deaa4d0648db47ec881fc0f
kyle <761473987@qq.com>
2020-09-01 23:03:31 +0800

    加入dashboard监控页面

--- springcloud-provider-dept/src/main/java/com/vsofo/springcloud/DeptApplication.java
+++ springcloud-provider-dept/src/main/java/com/vsofo/springcloud/DeptApplication.java
@@ -1,9 +1,12 @@
 package com.vsofo.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class DeptApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeptApplication.class, args);
+    }
+
+    //增加一个servlet
+    @Bean
+    public ServletRegistrationBean getServletBean () {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+                registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
     }
 }
